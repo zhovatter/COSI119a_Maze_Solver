b0e52658a75e69cd7a5ee0b11424a7a7d8ab3b41
zhovatter <123821138+zhovatter@users.noreply.github.com>
2024-10-11 13:39:20 -0400

    Update pid.py

--- pid.py
+++ pid.py
@@ -4,6 +4,7 @@ import numpy as np
 
 class PID:
     def __init__(self, min_val, max_val, kp, ki, kd):
+        """A basic PID controller"""
         self.min_val = min_val
         self.max_val = max_val
         self.kp = kp
@@ -13,20 +14,21 @@ class PID:
         self.prev_error = 0
 
     def compute(self, setpoint, measured_value):
+        """computes the PID value based on constants and error"""
         error = setpoint - measured_value
+
+        #the integral term tended to rise too quickly so I capped it at 1 or -1
         if (abs(self.integral) <= 1):
             self.integral += error
         elif self.integral > 0: self.integral = 1
         else: self.integral = 1
+
         self.derivative = error - self.prev_error
+
         if setpoint == 0 and self.prev_error == 0:
             self.integreal = 0
-        self.prev_error = error #previously this was after the pid calculation
-        # if abs(error) > 0.3:
-        #     error = 0.3 * (error/abs(error))
+        self.prev_error = error
+
         pid = self.kp * error + self.ki*self.integral + self.kd * self.derivative
-        print('error: ', error)
-        # print('int: ', self.integral)
-        # print('der: ', self.derivative)
-        print('PID: ', pid)
+
         return np.clip(pid, self.min_val, self.max_val)
